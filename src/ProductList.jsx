0affeb11326faf402f6da84ea0fd7ab5d0ff18af
RobbieFleming <robert.fleming96@outlook.com>
2025-09-17 04:11:26 -0400

    "Fix: Added base path for GitHub Pages"

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -226,7 +226,7 @@ function ProductList({ onHomeClick }) {
         padding: '15px',
         display: 'flex',
         justifyContent: 'space-between',
-        alignIems: 'center',
+        alignItems: 'center',
         fontSize: '20px',
     }
     const styleObjUl = {
@@ -285,21 +285,35 @@ return (
                     </div>
 
                 </div>
-                <div style={styleObjUl}>
+            <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
-                </div>
-            </div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                    <h1 className='cart' style={{ position: "relative" }}>
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                         <rect width="156" height="156" fill="none"></rect>
+                           <circle cx="80" cy="216" r="12"></circle>
+                             <circle cx="184" cy="216" r="12"></circle>
+                              <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                               </svg>
+                    {calculateTotalQuantity() > 0 && (
+                <span className="cart-number">{calculateTotalQuantity()}</span>
+             )}
+           </h1>
+        </a>
+    </div>
+  </div>
+</div>
+
             {!showCart ? (
                 <div className="product-grid">
                   {plantsArray.map((category, index) => ( // Loop through each category in plantsArray
                     <div key={index}> {/* Unique key for each category div */}
                     <h1>
-                    <div>{category.category}</div> {/* Display the category name */}
+                    {category.category} {/* Display the category name */}
                     </h1>
                     <div className="product-list"> {/* Container for the list of plant cards */}
                     {category.plants.map((plant, plantIndex) => { // Loop through each plant in the current category
-                    const isAdded = cartItems.some(item => item.name === plant.name);
+                    const isAdded = CartItems.some(item => item.name === plant.name);
                    return (
                    <div className="product-card" key={plantIndex}> {/* Unique key for each plant card */}
                      <img 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping",
+  base: "/e-plantShopping/",
   plugins: [react()],
-})
+
+  '/api': {
+    target: 'http://localhost:8888',
+    changeOrigin: true,
+    secure: false,
+  },
+},
+)
